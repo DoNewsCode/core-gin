0b8a5dc2dd0cad16ef68d70c38310bf70811cdb3
Reasno <guxi99@gmail.com>
2021-03-12 18:22:11 +0800

    feat: first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # core-gin
+
+This repository hosts packages for Gin integration for  ❤️ package [Core](https://github.com/DoNewsCode/core).
---
+++ mw/trace.go
@@ -0,0 +1,18 @@
+package mw
+
+import (
+	"net/http"
+
+	"github.com/DoNewsCode/core/contract"
+	"github.com/DoNewsCode/core/key"
+	"github.com/gin-gonic/gin"
+	tracegin "github.com/opentracing-contrib/go-gin/ginhttp"
+	stdtracing "github.com/opentracing/opentracing-go"
+)
+
+// Trace is a gin middleware that adds opentracing support.
+func Trace(tracer stdtracing.Tracer, keyer contract.Keyer) gin.HandlerFunc {
+	return tracegin.Middleware(tracer, tracegin.OperationNameFunc(func(r *http.Request) string {
+		return key.KeepOdd(keyer).Key(".", "http", r.Method)
+	}))
+}
